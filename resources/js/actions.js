6abb5adfa948de99bbb3cbdedb607fea4f71ff0b
heronothero <115736221+heronothero@users.noreply.github.com>
2024-04-04 12:48:42 +0700

    Update actions.js

--- resources/js/actions.js
+++ resources/js/actions.js
@@ -1,15 +1,3 @@
-/*const flipper = document.querySelector('.flipper');
-const btn = flipper.querySelector('.bttn');
-const results = flipper.querySelector('.results button');
-
-
-btn.addEventListener('click', () => {
-  flipper.classList.toggle('flip');
-});
-
-results.addEventListener('click', () => {
-  flipper.classList.toggle('flip');
-});*/
 document.addEventListener('DOMContentLoaded', function() {
   const flipper = document.querySelector('.flipper');
   const btn = flipper.querySelector('.bttn');
